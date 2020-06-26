b3a08f632df4923efe271cff2591536ec29e33e1
Ayushmaan Seth <ayushmaanseth@gmail.com>
2020-06-26 11:16:33 +0530

    changed domain

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,9 +1,9 @@
-// TODO Add a couple lines about each project
+
 const data = [
   {
     title: 'MpFree',
     subtitle: 'Self-Project',
-    image: '../images/projects/song.jpg',
+    image: 'images/projects/song.jpg',
     desc: 'Currently developing a cross-platform application for downloading songs easily and freely with music recognition, distributed and scaled streaming service.'
       + ' Will have integration with services like Spotify and Apple Music for bulk downloads.'
     
@@ -11,7 +11,7 @@ const data = [
     title: 'BlackBox Insight',
     subtitle: '',
     link: 'https://github.com/Ayushmaanseth/BlackBox_Insight',
-    image: '../images/projects/blackbox.png',
+    image: 'images/projects/blackbox.png',
     date: '2019-03-01',
     desc: 'Brainstormed and developed an online platform to explain and interpret in lay terms black-box algorithms'
       +  ' used in Investment Banking, Health-care and Government sector to solve the problem of making AI ”Transparent and Fair” '
@@ -19,7 +19,7 @@ const data = [
     title: 'RewardRush',
     subtitle: '',
     link: 'https://adc.bmj.com/content/103/Suppl_2/A50.2',
-    image: '../images/projects/rewardrush.png',
+    image: 'images/projects/rewardrush.jpg',
     date: '2018-06-01',
     desc: 'Developed an Augmented Reality powered mobile application (App) to improve patient experience at GOSH'
       + 'Used Unity3D for development, Vuforia SDK for marker-based AR, scripting in C\#}'
